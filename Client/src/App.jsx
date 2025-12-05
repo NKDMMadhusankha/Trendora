ade0cc7a7e347f866c0e25c3291062d6d93536b6
Mithila <150042569+NKDMMadhusankha@users.noreply.github.com>
2025-12-06 02:25:59 +0530

    Add Women's Wear page with filtering and sorting
    Introduces WomensPage.jsx, displaying women's products with filter and sort options. Updates App.jsx to route '/womens' to the new page.

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './Pages/HomePage'
 import MensPage from './Pages/MensPage'
+import WomensPage from './Pages/WomensPage'
 import Login from './Components/Login'
 import toast from 'react-hot-toast'
 import Register from './Components/Register'
@@ -11,6 +12,7 @@ const App = () => {
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/mens" element={<MensPage />} />
+      <Route path="/womens" element={<WomensPage />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
     </Routes>
